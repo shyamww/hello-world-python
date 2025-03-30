8fa4028efefcd18061bfac7e5b66d2764c03f2d4
W <w@Shyams-MacBook-Pro.local>
2025-03-30 22:16:40 +0530

    Add Hello World script

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    """Print a welcome message to the console."""
+    print("Hello, World!")
+    
+    # Add a little extra message
+    print("Welcome to my first Python project on GitHub!")
+
+if __name__ == "__main__":
+    main()
